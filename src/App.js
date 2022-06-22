a3c40a259ef86283d15d4a751ad51ebbd6869045
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-06-22 18:28:15 +0300

    Setting up the routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+// Importing routing elements from react-router-dom to use the page routing further
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from './pages/NewMeetup'
+import FavouritesPage from "./pages/Favourites"
+
 function App() {
   return (
     <div>
+      <Routes>
+        {/* Using the Routes component to navigate between routes */}
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
     </div>
   );
 }
