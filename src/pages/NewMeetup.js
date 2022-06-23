df2288004b8a1035dfc71c0e0bf2b264e6db0485
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-06-23 17:34:43 +0300

    Implementing Navigate React Hook

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,11 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from "react-router-dom";
+// Importing the internal React hook to manipulate browser history later on
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+  // Creating a variable that stores useNavigate function
+
   function addMeetupHandler(meetupData) {
     // Creating a separate local function to handle the addMeetup & accepting meetupData as expected props
     fetch("https://react-crash-course-7cc99-default-rtdb.firebaseio.com/meetups.json",
@@ -15,7 +20,13 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
           // Creating the headers & specifying the content type 
         }
-      });
+      }
+    ).then(() => {
+      // As soon as the fetch promise resolves, we add another promise to handle the following event
+      navigate("/", { replace: true });
+      // We use navigate variable and pass in the path to "/" to return the user to the main page on sumbitting the data. 
+      // ! It's important to put {replace: true} as a second parameter to replace the previous url with '/'
+    })
   }
   return (
     <section>
